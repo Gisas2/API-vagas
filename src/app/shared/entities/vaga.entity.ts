7c05387c1ac778710f0cc162c7357393a175162b
Gislene Marciano <gisa8485@hotmail.com>
2022-12-01 19:41:10 -0300

    atualizações no codigo

---
+++ src/app/shared/entities/vaga.entity.ts
@@ -0,0 +1,52 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { UserEntity } from "./user.entity";
+
+@Entity({
+    name: "vaga",
+})
+export class VagaEntity {
+    @PrimaryColumn()
+    id!: string;
+
+    @Column()
+    descricao: string;
+
+    @Column()
+    dtLimite: Date;
+
+    @Column()
+    indAtivo: boolean;
+
+    @Column()
+    maxCandidatos: number;
+
+    @Column({
+        name: "id_recrutador",
+    })
+    idRecrutador: string;
+
+    @CreateDateColumn({
+        name: "created_at",
+    })
+    create: Date;
+
+    @UpdateDateColumn({
+        name: "updated_at",
+    })
+    update: Date;
+
+    @ManyToOne(() => UserEntity)
+    @JoinColumn({
+        name: "id_recrutador",
+    })
+    recrutador: UserEntity;
+}
---
+++ src/main/database/typeorm.connection.ts
@@ -0,0 +1,21 @@
+import { DataSource } from "typeorm/data-source";
+import typeormConfig from "../config/typeorm.config";
+
+export class DatabaseConnection {
+
+    private static _connection: DataSource;
+
+    public static async connect() {
+        if(!this._connection) {
+            this._connection = await typeormConfig.initialize();
+        }
+        console.log('Database inicializada!')
+    }
+
+    public static get connection() {
+        if(!this._connection) {
+            throw new Error('Já está inicializado!')
+        }
+        return this._connection;
+    }
+}
